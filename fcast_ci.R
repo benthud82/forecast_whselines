048fa81f20d2795588af925a7455c0ca53cddccf
Bentley <benthud82@gmail.com>
2019-05-27 14:52:17 -0500

    fcast_ci

--- fcast_ci.R
+++ fcast_ci.R
@@ -1,34 +1,68 @@
 
 
-
-
 source('connections.R')
 source('RMySQL_Update.R')
 
 query <- function(...)
   dbGetQuery(mychannel, ...)
 
+
+#Generate list of dates
+
 sqlquery <- paste(
   "SELECT
-    fcast_lines
+  workday_date
   FROM
-    gillingham.forecast_whselines
+  gillingham.workdayofweek
   WHERE
-    fcast_date = '2019-01-03'
-    AND fcast_tier = 'BIN'",
+  workday_date BETWEEN '2019-05-01' AND '2019-05-31'
+  ORDER BY workday_date",
   sep = ""
 )
-data_lines <- query(sqlquery)
+data_dates <- query(sqlquery)
 
-testdata<-t.test(data_lines)
-ci_mean_named <- testdata$estimate
-ci_mean <- as.numeric(ci_mean_named)
-ci_lowbound <- testdata$conf.int[1]
-ci_upbound <- testdata$conf.int[2]
-
-ci_insert <-
-
-rmysql_update(mychannel,
-              forecast_insert,
-              'gillingham.forecast_whselines',
-              verbose = FALSE)
+for (i in 1:nrow(data_dates)) {
+  var_date <- (data_dates[i, ])
+  
+  sqlquery <- paste(
+    "SELECT
+    fcast_lines
+    FROM
+    gillingham.forecast_whselines_tier
+    WHERE
+    fcast_date = '",
+    var_date,
+    "'
+    AND fcast_tier = 'FLOW'",
+    sep = ""
+  )
+  data_lines <- query(sqlquery)
+  
+  testdata <- t.test(data_lines)
+  ci_mean_named <- testdata$estimate
+  ci_mean <- as.numeric(ci_mean_named)
+  ci_lowbound <- testdata$conf.int[1]
+  ci_upbound <- testdata$conf.int[2]
+  # #  ci_insert <- list()
+  # ci_insert <- as.data.frame(
+  #   #date = as.Date(character()),
+  #   #tier = character(),
+  #   #mean = integer(),
+  #   #lower = integer(),
+  #   #upper = integer()
+  # )
+  # ci_insert$date <- var_date
+  # ci_insert$tier <- 'FLOW'
+  # ci_insert$mean <- ci_mean
+  # ci_insert$lower <- ci_lowbound
+  # ci_insert$upper <- ci_upbound
+  
+  ci_insert <- data.frame(var_date, 'FLOW', ci_mean, ci_lowbound, ci_upbound)
+  
+  rmysql_update(mychannel,
+                ci_insert,
+                'gillingham.fcast_ci',
+                verbose = FALSE)
+  
+}
+lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
